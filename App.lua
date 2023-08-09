9c5eb3795fbcdc8aae235db9bf0d72a209936e96
jadissa <jadissag@gmail.com>
2023-08-09 00:40:09 -0600

    notify

--- App.lua
+++ App.lua
@@ -5,6 +5,41 @@ Addon.APP:RegisterEvent( 'ADDON_LOADED' );
 Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
     if( AddonName == 'jReadyCheck' ) then
 
+        Addon.APP.Theme = {
+            Notify = {
+                Hex = '009bff',
+            },
+        };
+
+        Addon.APP.Notify = function( self,... )
+            local prefix = CreateColor(
+                self.Theme.Notify.r,
+                self.Theme.Notify.g,
+                self.Theme.Notify.b
+            ):WrapTextInColorCode( AddonName );
+
+            _G[ 'DEFAULT_CHAT_FRAME' ]:AddMessage( string.join( ' ', prefix, ... ) );
+        end
+
+        --
+        --  Module init
+        --
+        --  @return void
+        Addon.APP.Init = function( self )
+
+            self.Name = AddonName;
+
+            for Key,Data in pairs( self.Theme ) do
+                if( Key ~= 'Font' ) then
+                    self.Theme[ Key ].r,self.Theme[ Key ].g,self.Theme[ Key ].b = Addon:Hex2RGB( Data.Hex );
+                end
+            end
+        end
+
+        --
+        --  Module run
+        --
+        --  @return void
         Addon.APP.Run = function( self )
             SOUNDKIT.READY_CHECK_OLD = SOUNDKIT.READY_CHECK;
             SOUNDKIT.READY_CHECK = SOUNDKIT.RAID_WARNING;
@@ -12,7 +47,11 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
                 local Frame = _G[ 'PartyMemberFrame'..Index ];
                 Frame:SetScript( 'OnEvent',function( self,Event,... )
                     if( Event == 'READY_CHECK' ) then
-                        UIErrorsFrame:AddMessage( 'You responded ready to leader' );
+                        local MessageText = 'You responded ready to leader';
+                        if( Index == 1 ) then
+                            UIErrorsFrame:AddMessage( MessageText );
+                            Addon.APP:Notify( MessageText );
+                        end
                         ReadyCheckFrameYesButton:Click();
                         C_Timer.After( 2,function()
                             ConfirmReadyCheck( true );
@@ -22,6 +61,7 @@ Addon.APP:SetScript( 'OnEvent',function( self,Event,AddonName )
             end
         end
 
+        self:Init();
         self:Run();
         self:UnregisterEvent( 'ADDON_LOADED' );
     end
